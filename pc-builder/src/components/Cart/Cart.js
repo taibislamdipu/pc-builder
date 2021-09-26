1bd93c3d42e9d91e664e50213c4824a6908caf91
taibislamdipu <mailtaibislam@gmail.com>
2021-09-26 18:46:56 +0600

    cart redesign

--- pc-builder/src/components/Cart/Cart.js
+++ pc-builder/src/components/Cart/Cart.js
@@ -10,18 +10,21 @@ const Cart = (props) => {
     const totalCost = cart.reduce((sum, item) => sum + item.price, 0)
 
     return (
-        <div className="">
+        <div>
             <h3>Product Added: {cart.length}</h3>
             <h4>Total Cost: ${totalCost}</h4>
             <hr />
-            {
-                cart.map(item =>
-                    <ul>
-                        <li>{item.name}</li>
-                        <img className="cart-image" src={item.image} alt="" /> = ${item.price}
-                    </ul>
-                )
-            }
+            <div >
+                <h5 className="text-center">Your selected items</h5>
+                {
+                    cart.map(item =>
+                        <div className="cart-item">
+                            <p>{item.name}</p>
+                            <img className="cart-image" src={item.image} alt="" /> = ${item.price}
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 };
